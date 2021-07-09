6f6f9317cd81304df12a76e21fc09cb09fff73d5
siva <siva636@gmail.com>
2021-07-09 06:02:53 +0530

    FlutterFire setup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_provider_get_it_firebase/main.dart';
+import 'package:provider_get_it/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
